fa3e83b3ba33ff0b57e7f5627b187f611f30f4bf
mamun-prodhan <mamunpr9@gmail.com>
2023-10-04 08:48:50 +0600

    first commmit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 style={{ color: "red" }}>Vite + React</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBOoBoNoKFzg0OAsjzDGwHsvkyugw7XoLo",
+  authDomain: "simple-firebase-c80ab.firebaseapp.com",
+  projectId: "simple-firebase-c80ab",
+  storageBucket: "simple-firebase-c80ab.appspot.com",
+  messagingSenderId: "424147254272",
+  appId: "1:424147254272:web:c81cd10cac5220567b6ce5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
